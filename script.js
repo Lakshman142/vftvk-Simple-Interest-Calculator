ffb75813348d085d82f51f5b49cab8b0c010d703
Lakshman Sai kumar <lakshman4345@gmail.com>
2022-07-31 10:17:21 +0000

    changes done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    var i= (p*t*(r/100));
+    var total=p+i;
+    if (p <= 0 | p== null) 
+    {
+         alert("Enter a positive number");
+         document.getElementById("principal").focus();
+         return false;
+    }
+    var year=new Date().getFullYear() + parseInt(t);
+    document.getElementById("result").innerHTML=`<div>If you deposit <span id="highlight" >${p}</span> <br>,at an interest rate of  <span id="highlight" >${r}</span> %.<br>You will receive an amount of <span id="highlight" >${total}</span> ,<br>in the year  <span id="highlight" >${year}</span> <br></div>`;
+}
+function updateRate()
+{
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval;
 }
         
